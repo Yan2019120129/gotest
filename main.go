63906b162c86f434e61585303499b27c7d1959d1
Yan2019120129 <1556403682@qq.com>
2023-12-04 00:51:14 +0800

    测试随机生成数据

--- main.go
+++ main.go
@@ -1,57 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/brianvoe/gofakeit/v6"
+	"time"
+)
 
-// Component 接口定义了组合中的基本操作
-type Component interface {
-	Show()
-}
-
-// MenuItem 表示菜单项
-type MenuItem struct {
-	Name string
-}
-
-// Show 实现了 Component 接口的 Show 方法
-func (m *MenuItem) Show() {
-	fmt.Printf("菜单项：%s\n", m.Name)
-}
-
-// Menu 表示菜单，可以包含菜单项和子菜单
-type Menu struct {
-	Name       string
-	Components []Component
-}
-
-// Show 实现了 Component 接口的 Show 方法，遍历显示所有菜单项和子菜单
-func (m *Menu) Show() {
-	fmt.Printf("菜单：%s\n", m.Name)
-	for _, component := range m.Components {
-		component.Show()
-	}
-}
-
-// Add 添加菜单项或子菜单
-func (m *Menu) Add(c Component) {
-	m.Components = append(m.Components, c)
+// User 用户表
+type User struct {
+	Id          int     `fake:"-"`
+	AdminId     int     `fake:"{randomint:[1]}"`
+	ParentId    int     `fake:"{randomint:[1]}"`
+	UserName    string  `fake:"{name}"`
+	NickName    string  `fake:"{name}"`
+	Email       string  `fake:"{email}"`
+	Telephone   string  `fake:"{phone}"`
+	Avatar      string  `fake:"{imageurl}"`
+	Sex         int     `fake:"{randomint:[1,2]}"`
+	Birthday    int     `fake:"{number:946656000,1104508800}"`
+	Password    string  `fake:"{randomstring:['1234567890','qwertyuiopasdfghjklzxcvbnm','QWERTYUIOPASDFGHJKLZXCVBNM']}"`
+	SecurityKey string  `fake:"{randomstring:['1234567890','qwertyuiopasdfghjklzxcvbnm','QWERTYUIOPASDFGHJKLZXCVBNM']}"`
+	Money       float64 `fake:"{float64range:100,1000}"`
+	Type        int     `fake:"{randomint:[-2,-1,10]}"`
+	Status      int     `fake:"{randomint:[-2,-1,10]}"`
+	Data        string  `fake:"{loremipsumsentence}"`
+	UpdatedAt   int     `fake:"{day}"`
+	CreatedAt   int     `fake:"{day}"`
 }
 
 func main() {
-	// 创建菜单项
-	item1 := &MenuItem{Name: "菜单项1"}
-	item2 := &MenuItem{Name: "菜单项2"}
+	// 初始化配置
+	currentTime := time.Now().Unix()
+	userInfo := new(User)
+	err := gofakeit.Struct(userInfo)
+	userInfo.UpdatedAt = int(currentTime)
+	userInfo.CreatedAt = int(currentTime)
+	if err != nil {
+		panic(err)
+	}
 
-	// 创建子菜单
-	submenu := &Menu{Name: "子菜单"}
-	submenu.Add(&MenuItem{Name: "子菜单项1"})
-	submenu.Add(&MenuItem{Name: "子菜单项2"})
-
-	// 创建主菜单，包含菜单项和子菜单
-	menu := &Menu{Name: "主菜单"}
-	menu.Add(item1)
-	menu.Add(item2)
-	menu.Add(submenu)
-
-	// 显示整个菜单系统
-	menu.Show()
+	fmt.Println("userInfo:", userInfo)
 }
