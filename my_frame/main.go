9bef2884a26d84b0f49075e562e0f7b2251ff81b
Yan2019120129 <1556403682@qq.com>
2023-12-13 03:41:53 +0800

    商户管理员初始化代码简化

--- my_frame/main.go
+++ my_frame/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"gotest/my_frame/config"
-	esearch "gotest/my_frame/config/elasticsearch"
+	"gotest/my_frame/config/database"
 	"gotest/my_frame/test"
 )
 
@@ -12,16 +12,44 @@ func main() {
 	cfg := config.GetConfig()
 
 	// 初始化配置
-	//database.Init(&cfg.Database)
+	database.Init(&cfg.Database)
 
 	// 初始化redis
 	//redis.Init(&cfg.Redis)
 
 	// 初始化Elasticsearch
-	esearch.Init(&cfg.Elasticsearch)
+	//esearch.Init(&cfg.Elasticsearch)
 
 	//// 初始化gin
 	//gin.Init(&cfg.Gin)
 
-	test.DeleteIndex("customer")
+	//test.TestTime()
+	//test.TestMap()
+
+	//err := test.Synchronous([]*test.SynchronousParams{
+	//	{Table: "wallet_assets", AdminId: 2, WhereField: []string{"name"}},
+	//	{Table: "lang", AdminId: 2, WhereField: []string{"name", "alias"}},
+	//	{Table: "article", AdminId: 2, WhereField: []string{"name", "type"}},
+	//	{Table: "level", AdminId: 2, WhereField: []string{"name", "money"}},
+	//	{Table: "real_name_auth", AdminId: 2, WhereField: []string{"real_name", "number"}},
+	//	{Table: "translate", AdminId: 2, WhereField: []string{"name", "field", "value"}},
+	//	{Table: "admin_setting", AdminId: 2, WhereField: []string{"name", "type", "field"}},
+	//})
+	//if err != nil {
+	//	panic(err)
+	//}
+
+	err := test.Copy([]*test.CopyParams{
+		{Table: "wallet_assets", AdminId: 2},
+		{Table: "lang", AdminId: 2},
+		{Table: "article", AdminId: 2},
+		{Table: "level", AdminId: 2},
+		{Table: "real_name_auth", AdminId: 2},
+		{Table: "translate", AdminId: 2},
+		{Table: "admin_setting", AdminId: 2},
+	})
+	if err != nil {
+		panic(err)
+	}
+
 }
