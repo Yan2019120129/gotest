c08208f97e69821c3379996bf3b18b57bf31bd57
Yan2019120129 <1556403682@qq.com>
2023-12-22 19:52:15 +0800

    添加控制版本，初始化包测试

---
+++ test/index.go
@@ -0,0 +1,12 @@
+package test
+
+import "gotest/test/test_init"
+
+var Success string
+
+func main() {
+}
+
+func init() {
+	Success = test_init.Ok
+}
---
+++ test/test_init/testInit.go
@@ -0,0 +1,7 @@
+package test_init
+
+var Ok string
+
+func init() {
+	Ok = "ok"
+}
