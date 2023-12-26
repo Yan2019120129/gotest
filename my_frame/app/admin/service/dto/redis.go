507edfbc5c4896af535865e6eb4fa01d6d9c7e3d
Yan2019120129 <1556403682@qq.com>
2023-12-26 18:03:17 +0800

    未完成redis订阅发布

--- my_frame/app/admin/service/dto/redis.go
+++ my_frame/app/admin/service/dto/redis.go
@@ -4,5 +4,13 @@ package dto
 type RdsPublishAndParams struct {
 	Channel string `json:"channel"` // 订阅通道
 	Message string `json:"message"` // 内容
-	Type    string `json:"type"`    // 类型 1发布 2订阅
+	Type    int    `json:"type"`    // 类型 1发布 2订阅
+}
+
+// RdsParams 设置redis参数
+type RdsParams struct {
+	Command string `json:"command"` // 命令
+	Type    int    `json:"type"`    // 类型 1添加 2获取
+	Key     string `json:"key"`     // 键
+	Value   string `json:"value"`   // 值
 }
--- my_frame/app/admin/service/index/subscription.go
+++ my_frame/app/admin/service/index/subscription.go
@@ -1,11 +1,42 @@
 package index
 
-import "gotest/my_frame/module/redis"
+import (
+	"context"
+	"fmt"
+	"github.com/gomodule/redigo/redis"
+	"gotest/my_frame/app/admin/service/dto"
+	"gotest/my_frame/module/cache"
+)
 
 // SubRds 订阅信息
-func SubRds() (interface{}, error) {
-	rdsConn := redis.RdsPubSubConn
+func SubRds(params *dto.RdsPublishAndParams) (interface{}, error) {
+	rdsConn := cache.RdsPubSubConn
 	defer rdsConn.Close()
-	rdsConn.Subscribe()
-	return nil, nil
+	ctx := context.Background()
+	if err := rdsConn.Subscribe(ctx, params.Channel); err != nil {
+		return nil, err
+	}
+	go func() {
+		for {
+			switch v := rdsConn.Receive().(type) {
+			case redis.Message:
+				fmt.Println("data:", v.Channel, v.Data)
+			case redis.Subscription:
+				fmt.Println("订阅:", v.Count)
+			}
+		}
+	}()
+	return "ok", nil
+}
+
+// Publish 发布信息
+func Publish(params *dto.RdsPublishAndParams) (interface{}, error) {
+	rdsConn := cache.RdsPubSubConn
+	defer rdsConn.Close()
+	ctx := context.Background()
+	if err := rdsConn.PUnsubscribe(ctx, params.Channel, params.Message); err != nil {
+		return nil, err
+	}
+
+	return "ok", nil
 }
