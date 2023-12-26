43d4c53f9474cf2a40a6ec202808c8ab9defb472
Yan2019120129 <1556403682@qq.com>
2023-12-26 14:38:41 +0800

    整理redis配置

--- my_frame/config/config.go
+++ my_frame/config/config.go
@@ -1,6 +1,12 @@
 package config
 
-import "gorm.io/gen"
+import (
+	"fmt"
+	"gopkg.in/yaml.v3"
+	"gorm.io/gen"
+	"os"
+	"sync"
+)
 
 const (
 	DatabaseTypeMysql      = "mysql"
@@ -14,8 +20,8 @@ var GenMdoe = map[string]interface{}{
 }
 
 const (
-	//FilePath = "/home/programmer-yan/Documents/GoFile/gotest/my_frame/config/config.yml"
-	FilePath = "/Users/taozi/Documents/Golang/gotest/my_frame/config/config.yml"
+	FilePath = "/home/programmer-yan/Documents/GoFile/gotest/my_frame/config/config.yml"
+	//FilePath = "/Users/taozi/Documents/Golang/gotest/my_frame/config/config.yml"
 )
 
 type Config struct {
@@ -73,6 +79,12 @@ type DatabaseConfig struct {
 
 // RedisConfig redis配置
 type RedisConfig struct {
+	UsePub bool            `yaml:"use-pub"` // 是否配置redis订阅功能
+	Poll   RedisPollConfig `yaml:"poll"`    // 连接池配置
+}
+
+// RedisPollConfig redisPool 连接池连接配置
+type RedisPollConfig struct {
 	Network         string `yaml:"network"`            // 连接协议
 	Server          string `yaml:"server"`             // 服务地址
 	Port            int    `yaml:"port"`               // 端口号
@@ -85,4 +97,64 @@ type RedisConfig struct {
 	ConnMaxIdleTime int    `yaml:"conn-max-idle-time"` // 空闲连接数
 	MaxIdleConn     int    `yaml:"max-idle-conn"`      // 最大空闲连接数
 	Wait            bool   `yaml:"wait"`               // 如果超时最大连接数是否等待
+}
+
+// once 用于初始化config变量，并保证只初始化一次
+var _once sync.Once
+
+// 定义全局变量config，并初始化为nil
+var cfg *Config
+
+func init() {
+	if cfg == nil {
+		_once.Do(
+			func() {
+				if configByte, err := os.ReadFile(FilePath); err == nil {
+					if err = yaml.Unmarshal(configByte, &cfg); err != nil {
+						panic(err)
+					}
+					fmt.Printf("内存地址：%p----->配置文件初始化成功！！！\n", cfg)
+				} else {
+					panic(err)
+				}
+			},
+		)
+	} else {
+		fmt.Println("配置文件实例已存在！！！")
+	}
+}
+
+// GetGorm  获取gorm 配置
+func GetGorm() *GormConfig {
+	return &cfg.Gorm
+}
+
+// GetGen  获取gen 配置
+func GetGen() *GenConfig {
+	return &cfg.Gorm.GenConfig
+}
+
+// GetMysql  获取mysql 配置
+func GetMysql() *DatabaseConfig {
+	return &cfg.Gorm.Database.Mysql
+}
+
+// GetPostgres  获取postgres 配置
+func GetPostgres() *DatabaseConfig {
+	return &cfg.Gorm.Database.Postgresql
+}
+
+// GetGin  获取gin 配置
+func GetGin() *GinConfig {
+	return &cfg.Gin
+}
+
+// GetRedisConfig  获取redis 配置
+func GetRedisConfig() *RedisConfig {
+	return &cfg.Redis
+}
+
+// GetElasticsearch  获取elasticsearch 配置
+func GetElasticsearch() *ElasticsearchConfig {
+	return &cfg.Elasticsearch
 }
