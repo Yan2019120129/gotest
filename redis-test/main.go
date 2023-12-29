e859758c5be8676fbae799bd6bcc7b9eb53e7e0f
Yan2019120129 <1556403682@qq.com>
2023-12-29 15:49:52 +0800

    添加redis 发布订阅测试

--- redis-test/main.go
+++ redis-test/main.go
@@ -2,63 +2,32 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"github.com/redis/go-redis/v9"
 )
 
 // redis连接对象
 var client *redis.Client
+var ctx = context.Background()
 
-func main() {
-	TestHset()
-}
-
-// TestHset  插入哈希表
-func TestHset() {
-	MDTUSDT := map[string]string{
-		"MDT-USDT": `{
-			"instType":  "SPOT",
-			"instId":    "MDT-USDT",
-			"last":      "9999.99",
-			"lastSz":    "1",
-			"askPx":     "9999.99",
-			"askSz":     "11",
-			"bidPx":     "8888.88",
-			"bidSz":     "5",
-			"open24h":   "9000",
-			"high24h":   "10000",
-			"low24h":    "8888.88",
-			"volCcy24h": "2222",
-			"vol24h":    "2222",
-			"sodUtc0":   "0.1",
-			"sodUtc8":   "0.1",
-			"ts":        "1597026383085",
-		}`,
-	}
-	ctx := context.Background()
-	if err := client.HSet(ctx, "SPOT", MDTUSDT).Err(); err != nil {
-		panic(err)
-	}
-
-	if err := client.HGetAll(ctx, "SPOT").Err(); err != nil {
-		panic(err)
-	}
-}
-
-// TestHdel  删除哈希表
-func TestHdel() {
-	ctx := context.Background()
-	key := "SPOT"
-	if err := client.Del(ctx, key).Err(); err != nil {
-		panic(err)
-	}
-	spot := client.HGetAll(ctx, key)
-	result, err := spot.Result()
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("result:", result)
+var MDTUSDT = map[string]string{
+	"instType":  "SWAP",
+	"instId":    "LTC-USD-SWAP",
+	"last":      "9999.99",
+	"lastSz":    "1",
+	"askPx":     "9999.99",
+	"askSz":     "11",
+	"bidPx":     "8888.88",
+	"bidSz":     "5",
+	"open24h":   "9000",
+	"high24h":   "10000",
+	"low24h":    "8888.88",
+	"volCcy24h": "2222",
+	"vol24h":    "2222",
+	"sodUtc0":   "0.1",
+	"sodUtc8":   "0.1",
+	"ts":        "1597026383085",
 }
 
 // init 初始化redis
@@ -68,4 +37,76 @@ func init() {
 		Password: "", // 没有密码，默认值
 		DB:       0,  // 默认DB 0
 	})
+}
+
+func main() {
+	Publish()
+}
+
+// Hset 插入hash数据
+func Hset() {
+
+	data, err := json.Marshal(&MDTUSDT)
+	if err != nil {
+		panic(err)
+	}
+	key := "SPOT"
+	if err = client.HSet(ctx, key, data).Err(); err != nil {
+		panic(err)
+	}
+
+	if err = client.Close(); err != nil {
+		panic(err)
+	}
+}
+
+// Hdel 删除hash数据
+func Hdel() {
+	key := "SPOT"
+	if err := client.Del(ctx, key).Err(); err != nil {
+		panic(err)
+	}
+	spot := client.HGetAll(ctx, key)
+	result, err := spot.Result()
+	if err != nil {
+		panic(err)
+	}
+	if err = client.Close(); err != nil {
+		panic(err)
+	}
+	fmt.Println("result:", result)
+}
+
+// Publish 发布消息
+func Publish() {
+	channel := "SPOT"
+
+	data, err := json.Marshal(&MDTUSDT)
+	if err != nil {
+		panic(err)
+	}
+	if err = client.Publish(ctx, channel, data).Err(); err != nil {
+		panic(err)
+	}
+	for {
+		var msg string
+		if _, err = fmt.Scan(&msg); err != nil {
+			panic(err)
+		}
+		if err = client.Publish(ctx, channel, msg).Err(); err != nil {
+			panic(err)
+		}
+	}
+}
+
+// Subscribe 订阅消息
+func Subscribe() {
+	channel := "SPOT"
+	pubsub := client.Subscribe(ctx, channel)
+	defer pubsub.Close()
+	ch := pubsub.Channel()
+	for msg := range ch {
+		fmt.Println("channel:", msg.Channel)
+		fmt.Println("payload:", msg.Payload)
+	}
 }
