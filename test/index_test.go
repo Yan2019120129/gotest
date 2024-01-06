0075bccc533a98c48c1c8c32fcdbf4de2510e240
Yan2019120129 <1556403682@qq.com>
2024-01-06 11:09:37 +0800

    添加随机数文件

---
+++ test/index_test.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"fmt"
+	"github.com/google/uuid"
+	"gotest/my_frame/models"
+	"gotest/my_frame/module/gorm/database"
+	"gotest/my_frame/module/logger"
+	"gotest/my_frame/utils"
+	"strconv"
+	"testing"
+	"time"
+)
+
+// TestRandom 测试随机数
+func TestRandom(t *testing.T) {
+	array := []string{}
+	for i := 0; i < 1000; i++ {
+		array = append(array, utils.NewRandom().OrderSn())
+	}
+
+	testMap := map[string]string{}
+	for i := 0; i < len(array); i++ {
+		key := array[i]
+		if testMap[key] != "" {
+			logger.Logger.Debug("出现重复键:" + key + "-" + strconv.Itoa(i))
+			return
+		}
+		testMap[key] = strconv.Itoa(i)
+		logger.Logger.Debug("key:" + key)
+	}
+	logger.Logger.Debug("完成")
+}
+
+// TestTime 测试时间
+func TestTime(t *testing.T) {
+	nowTime := time.Now()
+	logger.Logger.Debug(strconv.Itoa(int(nowTime.Unix())))
+	logger.Logger.Debug(strconv.Itoa(int(nowTime.UnixMilli())))
+	logger.Logger.Debug(strconv.Itoa(int(nowTime.UnixNano())))
+	logger.Logger.Debug(strconv.Itoa(int(nowTime.UnixMicro())))
+}
+
+// TestTime 测试时间
+func TestArray(t *testing.T) {
+	a := make([]int, 5)
+
+	a[0] = 1
+	a[1] = 2
+	//a[2] = 3
+	//a[3] = 4
+	//a = append(a, 1)
+	//a = append(a, 2)
+	//a = append(a, 3)
+	//a = append(a, 4)
+	fmt.Println("a:", len(a))
+}
+
+// TestAutoTime 测试gorm 自动更新时间
+func TestAutoTime(t *testing.T) {
+	user := models.User{
+		AdminId:  1,
+		ParentId: 3,
+	}
+	database.DB.Create(&user)
+	fmt.Println(user.UpdatedAt)
+	fmt.Println(user.CreatedAt)
+}
+
+// TestUUID	 测试UUID
+func TestUUID(t *testing.T) {
+	newUUID := uuid.New().ID()
+	logger.Logger.Debug(strconv.Itoa(int(newUUID)))
+	uuidArray := []uint32{}
+	for i := 0; i < 1000; i++ {
+		uuidArray = append(uuidArray, uuid.New().ID())
+	}
+	testMap := map[uint32]string{}
+	for i := 0; i < len(uuidArray); i++ {
+		key := uuidArray[i]
+		if testMap[key] != "" {
+			logger.Logger.Debug("出现重复键:" + strconv.Itoa(int(key)) + "-" + strconv.Itoa(i))
+			return
+		}
+		testMap[key] = strconv.Itoa(i)
+		logger.Logger.Debug("key:" + strconv.Itoa(int(key)))
+	}
+	logger.Logger.Debug("完成")
+}
