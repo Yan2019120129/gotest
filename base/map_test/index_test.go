fccbace913964fac2d265ef010b84aa6c83530d4
Yan2019120129 <1556403682@qq.com>
2024-01-15 14:40:26 +0800

    添加map测试

---
+++ base/map_test/index_test.go
@@ -0,0 +1,21 @@
+package map_test_test
+
+import (
+	"gotest/base/map_test"
+	"testing"
+)
+
+// TestMap 测试map键值存储
+func TestMap(t *testing.T) {
+	map_test.TestMap()
+}
+
+// TestMap 测试map键值存储
+func TestForMap(t *testing.T) {
+	map_test.TestForMap()
+}
+
+// TestMap 测试map键值存储
+func TestIfMap(t *testing.T) {
+	map_test.TestIfMap()
+}
