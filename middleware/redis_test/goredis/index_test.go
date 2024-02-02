f4b6f88b7eee6644e7364e7038472ae1c2faa0a7
Yan2019120129 <1556403682@qq.com>
2024-02-02 10:59:57 +0800

    添加 goredis 日志测试

--- middleware/redis_test/goredis/index_test.go
+++ middleware/redis_test/goredis/index_test.go
@@ -1,7 +1,7 @@
-package redis_test_test
+package goredis_test
 
 import (
-	"gotest/middleware/redis_test"
+	"gotest/middleware/redis_test/goredis"
 	"sync"
 	"testing"
 	"time"
@@ -9,23 +9,23 @@ import (
 
 // Hset 插入哈希表
 func TestHset(t *testing.T) {
-	redis_test.Hset()
+	goredis.Hset()
 }
 
 // TestHdel  删除哈希键值
 func TestHdel(t *testing.T) {
-	redis_test.Hdel()
+	goredis.Hdel()
 }
 
 // TestPublish 发布信息
 func TestPublish(t *testing.T) {
-	redis_test.Publish()
+	goredis.Publish()
 }
 
 // TestSubscribe  订阅消息
 func TestSubscribe(t *testing.T) {
 	wg := sync.WaitGroup{}
-	go redis_test.Subscribe()
+	go goredis.Subscribe()
 	wg.Add(1)
 	wg.Wait()
 }
@@ -33,7 +33,7 @@ func TestSubscribe(t *testing.T) {
 // TestSubscribe1  订阅消息
 func TestSubscribe1(t *testing.T) {
 	wg := sync.WaitGroup{}
-	go redis_test.Subscribe()
+	go goredis.Subscribe()
 	wg.Add(1)
 	wg.Wait()
 }
@@ -41,7 +41,7 @@ func TestSubscribe1(t *testing.T) {
 // TestGet 获取数据
 func TestGetGet(t *testing.T) {
 	for {
-		redis_test.Get()
+		goredis.Get()
 		time.Sleep(3 * time.Second)
 	}
 }
