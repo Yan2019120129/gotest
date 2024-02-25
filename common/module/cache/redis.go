d940d9fdd1513b22b3561d5e2606fc03882ff6d6
Yan2019120129 <1556403682@qq.com>
2024-02-25 11:48:56 +0800

    修改公共方法

--- common/module/cache/redis.go
+++ common/module/cache/redis.go
@@ -18,7 +18,12 @@ var RdsPool *redis.Pool
 // RdsPubSubConn redis连接池单例,保证全局单例,保证全局单例,保证全局单例。
 var RdsPubSubConn *redis.PubSubConn
 
-// 获取redis配置
+type instance struct {
+	RdsPubSubConn *redis.PubSubConn // redis 实例
+	RdsPool       *redis.Pool
+}
+
+var Instance = &instance{}
 
 // Init 初始化Redis。
 func init() {
@@ -48,10 +53,12 @@ func init() {
 					return conn, nil
 				},
 			}
-
+			Instance.RdsPool = RdsPool
 			// 是否启用订阅
 			if cfg.UsePub {
 				RdsPubSubConn = &redis.PubSubConn{Conn: RdsPool.Get()}
+				Instance.RdsPubSubConn = RdsPubSubConn
+
 				fmt.Printf("内存地址：%p----->Rds开启订阅！！！\n", RdsPubSubConn)
 			}
 			fmt.Printf("内存地址：%p----->Rds连接池初始化成功！！！\n", RdsPool)
@@ -61,16 +68,40 @@ func init() {
 	}
 }
 
-// SubRds 订阅消息
-func SubRds(channel string, message []byte) {
+// Subscribe 订阅消息
+func (i *instance) Subscribe(fun func(message []byte, channel string), channels ...interface{}) error {
+	if err := i.RdsPubSubConn.Subscribe(redis.Args{}.Add(channels...)...); err != nil {
+		return err
+	}
+	for {
+		switch v := i.RdsPubSubConn.Receive().(type) {
+		case redis.Message:
+			fun(v.Data, v.Channel)
+		case redis.Subscription:
+			fmt.Printf("订阅频道: %s，订阅数量: %d\n", v.Channel, v.Count)
+		case error:
+			fmt.Println("接收消息错误:", v)
+			return v
+		}
+	}
+}
+
+// UnSubscribe 取消订阅
+func (i *instance) UnSubscribe(channels ...interface{}) error {
+	if err := i.RdsPubSubConn.Unsubscribe(redis.Args{}.Add(channels...)...); err != nil {
+		return err
+	}
+	return nil
 }
 
 // Publish 发布信息
-func Publish(channel string, message []byte) {
-	rdsConn := RdsPool.Get()
+func (i *instance) Publish(message []byte, channels ...interface{}) {
+	rdsConn := i.RdsPool.Get()
 	defer rdsConn.Close()
-	if err := rdsConn.Send("PUBLISH", channel, message); err != nil {
-		log.Println(err)
-		return
+	for _, v := range channels {
+		if err := rdsConn.Send("PUBLISH", v, message); err != nil {
+			log.Println(err)
+			return
+		}
 	}
 }
