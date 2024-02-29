3ec521d6e3fe0735febfa0fdfd5b4eac0cd1ddc3
Yan2019120129 <1556403682@qq.com>
2024-02-29 10:45:29 +0800

    添加二叉树测试案例

--- base/tree_test/index.go
+++ base/tree_test/index.go
@@ -5,7 +5,7 @@ import "fmt"
 // TreeNode 数节点
 type TreeNode struct {
 	K     int // 节点存储的值
-	V     string
+	Val   int
 	Left  *TreeNode // 左子树
 	Right *TreeNode // 右子树
 }
@@ -17,7 +17,7 @@ func SearchNode(val int, t *TreeNode, count *int) {
 	}
 	*count++
 	if t.K == val {
-		fmt.Printf("Found Node: (%d, %s)\n", t.K, t.V)
+		fmt.Printf("Found Node: (%d, %d)\n", t.K, t.Val)
 		return
 	}
 	//if val < t.k {
@@ -140,4 +140,43 @@ func (t *BTree) printInOrder(x *Node, l int) {
 			}
 		}
 	}
+}
+
+// InorderTraversal 二叉树 Morris 遍历算法中序遍历 时间复杂度 O(n)，空间复杂度O(1)
+func InorderTraversal(root *TreeNode) (res []int) {
+	for root != nil {
+		if root.Left != nil {
+
+			// predecessor 节点表示当前 root 节点向左走一步，然后一直向右走至无法走为止的节点
+			predecessor := root.Left
+			for predecessor.Right != nil && predecessor.Right != root {
+
+				// 有右子树且没有设置过指向 root，则继续向右走
+				predecessor = predecessor.Right
+			}
+			if predecessor.Right == nil {
+
+				// 将 predecessor 的右指针指向 root，这样后面遍历完左子树 root.Left 后，就能通过这个指向回到 root
+				predecessor.Right = root
+
+				// 遍历左子树
+				root = root.Left
+			} else { // predecessor 的右指针已经指向了 root，则表示左子树 root.Left 已经访问完了
+				res = append(res, root.Val)
+
+				// 恢复原样
+				predecessor.Right = nil
+
+				// 遍历右子树
+				root = root.Right
+			}
+		} else { // 没有左子树
+			res = append(res, root.Val)
+
+			// 若有右子树，则遍历右子树
+			// 若没有右子树，则整颗左子树已遍历完，root 会通过之前设置的指向回到这颗子树的父节点
+			root = root.Right
+		}
+	}
+	return
 }
--- base/tree_test/index_test.go
+++ base/tree_test/index_test.go
@@ -11,31 +11,65 @@ func TestSearchNode(t *testing.T) {
 	tree := &tree_test.TreeNode{
 		K: 30,
 		Left: &tree_test.TreeNode{
-			K: 28,
-			V: "q",
+			K:   28,
+			Val: 'q',
 			Left: &tree_test.TreeNode{
-				K: 27,
-				V: "w",
+				K:   27,
+				Val: 'w',
 			},
 			Right: &tree_test.TreeNode{
-				K: 26,
-				V: "e",
+				K:   26,
+				Val: 'e',
 			},
 		},
 		Right: &tree_test.TreeNode{
-			K: 31,
-			V: "r",
+			K:   31,
+			Val: 'r',
 			Left: &tree_test.TreeNode{
-				K: 32,
-				V: "t",
+				K:   32,
+				Val: 't',
 			},
 			Right: &tree_test.TreeNode{
-				K: 35,
-				V: "y",
+				K:   35,
+				Val: 'y',
 			},
 		},
 	}
 	i := 0
 	tree_test.SearchNode(27, tree, &i)
 	fmt.Println("count:", i)
+}
+
+// TestInorderTraversal 迭代的方式遍历二叉树
+func TestInorderTraversal(t *testing.T) {
+	sum := tree_test.InorderTraversal(&tree_test.TreeNode{
+		Val: 1,
+		Left: &tree_test.TreeNode{
+			Val: 2,
+			Left: &tree_test.TreeNode{
+				Val:   4,
+				Left:  nil,
+				Right: nil,
+			},
+			Right: &tree_test.TreeNode{
+				Val:   5,
+				Left:  nil,
+				Right: nil,
+			},
+		},
+		Right: &tree_test.TreeNode{
+			Val: 3,
+			Left: &tree_test.TreeNode{
+				Val:   6,
+				Left:  nil,
+				Right: nil,
+			},
+			Right: &tree_test.TreeNode{
+				Val:   7,
+				Left:  nil,
+				Right: nil,
+			},
+		},
+	})
+	fmt.Println(sum)
 }
