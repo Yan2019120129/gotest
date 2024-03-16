bdf85d7b9ee5f8818d6f749c68d2fa952174dcf7
Yan2019120129 <1556403682@qq.com>
2024-03-16 18:33:53 +0800

    添加mysql 多表链接创建测试

--- middleware/mysql_t/create_test.go
+++ middleware/mysql_t/create_test.go
@@ -19,7 +19,12 @@ func TestCreateTable(t *testing.T) {
 
 // CreateTable 插入数据
 func TestInserter(t *testing.T) {
-	err := database.DB.Create(models.GetDefaultUser().SetAdminUser(models.GetDefaultAdminUser())).Error
+	userList := make([]*models.User, 0)
+	for i := 0; i < 10; i++ {
+		userList = append(userList, models.GetDefaultUser())
+	}
+	adminUserInfo := models.GetDefaultAdminUser().SetUser(userList...)
+	err := database.DB.Create(adminUserInfo).Error
 	if err != nil {
 		logs.Logger.Error(logs.LogMsgApp, zap.Error(err))
 		return
@@ -28,7 +33,13 @@ func TestInserter(t *testing.T) {
 
 // TestInsertAssociation 插入数据并关联Id
 func TestAssociation(t *testing.T) {
-	err := database.DB.Create(models.GetDefaultUser().SetAdminId(1)).Error
+	//err := database.DB.Create(models.GetDefaultUser().SetAdminId(1)).Error
+	//if err != nil {
+	//	logs.Logger.Error(logs.LogMsgApp, zap.Error(err))
+	//	return
+	//}
+
+	err := database.DB.Create(models.GetDefaultUser().SetAdminId(1).SetParentId(2)).Error
 	if err != nil {
 		logs.Logger.Error(logs.LogMsgApp, zap.Error(err))
 		return
