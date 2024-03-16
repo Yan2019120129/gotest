793a29e4963fc3b92bf6708db1c069b33bccefd8
Yan2019120129 <1556403682@qq.com>
2024-03-16 15:46:48 +0800

    添加mysql 多表连接查询

--- middleware/mysql_t/dto/adminUser.go
+++ middleware/mysql_t/dto/adminUser.go
@@ -1,11 +1,18 @@
 package dto
 
-import "gotest/common/models"
+import (
+	"gotest/common/models"
+)
 
 type AdminUserInfo struct {
 	models.Model
-	Username string `json:"username"`
-	Nickname string `json:"nickname"`
-	Email    string `json:"email"`
-	Avatar   string `json:"avatar"`
+	Username string       `json:"username"`
+	Nickname string       `json:"nickname"`
+	Email    string       `json:"email"`
+	Avatar   string       `json:"avatar"`
+	Users    *[]*UserInfo `json:"users" gorm:"foreignKey:AdminUserId;"`
+}
+
+func (a *AdminUserInfo) TableName() string {
+	return "admin_user"
 }
