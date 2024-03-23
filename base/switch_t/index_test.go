eee2beb7c25e07ada4a900268884049849626e8b
Yan2019120129 <1556403682@qq.com>
2024-03-23 21:19:33 +0800

    添加switch 测试

--- base/switch_t/index_test.go
+++ base/switch_t/index_test.go
@@ -12,3 +12,21 @@ func TestSwitch(t *testing.T) {
 	fmt.Println(3 * time.Second)
 	fmt.Println(Switch(elapsed))
 }
+
+// TestSwitchBool 测试bool
+func TestSwitchBool(t *testing.T) {
+	month := 3
+	switch month {
+	case 3, 4, 5:
+		fmt.Printf("春天")
+		fallthrough
+	case 6, 7, 8:
+		fmt.Printf("夏天")
+		//fallthrough
+	case 9, 10, 11:
+		fmt.Printf("秋天")
+		fallthrough
+	case 12, 1, 2:
+		fmt.Printf("冬天")
+	}
+}
