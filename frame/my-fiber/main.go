9cb7137f695e81a37cc65a1af057248a4359080e
Yan2019120129 <1556403682@qq.com>
2024-04-08 21:14:33 +0800

    添加my-fiber简单代码

--- frame/my-fiber/main.go
+++ frame/my-fiber/main.go
@@ -1,1 +1,14 @@
-package my_fiber
+package main
+
+import "github.com/gofiber/fiber/v3"
+
+func main() {
+	app := fiber.New()
+	app.Get("/", func(c fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+
+	if err := app.Listen(":3000"); err != nil {
+		return
+	}
+}
