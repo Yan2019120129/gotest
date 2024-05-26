3274023eed989a6877d2710d5aba4ce35047b3a4
Yan2019120129 <1556403682@qq.com>
2024-05-27 00:36:07 +0800

    添加产品模版，产品模型

--- common/models/product.go
+++ common/models/product.go
@@ -1,67 +1,35 @@
 package models
 
-import "github.com/brianvoe/gofakeit/v6"
+import (
+	"gorm.io/gorm"
+)
+
+const (
+	ProductStatusActive  = 10
+	ProductStatusDisable = -1
+
+	// ProductTypeDefault 店铺商品类型
+	ProductTypeDefault = 1
+	// ProductTypeWholesale  批发商品类型
+	ProductTypeWholesale = 2
+)
 
 // Product 产品表
 type Product struct {
-	Id                int `gorm:" type:int unsigned primary key auto_increment; comment: 主键"`
-	AdminUserId       int `gorm:" type:int unsigned not null; comment: 管理员ID"`
-	AdminUser         *AdminUser
-	ProductCategoryId int `gorm:" type:int unsigned not null; comment: 类目ID"`
-	ProductCategory   *ProductCategory
-	WalletAssetsId    int `gorm:" type:int unsigned not null; comment: 资产ID"`
-	WalletAssets      *WalletAssets
-	Name              string  `gorm:" type:varchar(64) not null; comment: 标题"`
-	Images            string  `gorm:" type:varchar(2048) not null; comment: 图片列表"`
-	Money             float64 `gorm:" type:decimal(12,2) not null; comment: 金额"`
-	Type              int     `gorm:" type:tinyint not null; default: 1; comment: 类型 1默认"`
-	Sort              int     `gorm:" type:int unsigned not null; comment: 排序"`
-	Status            int     `gorm:" type:tinyint not null; default: 10; comment: 状态 -2删除 -1禁用 10启用"`
-	Recommend         int     `gorm:" type:tinyint not null; default: -1; comment: 推荐 -1关闭 10推荐"`
-	Sales             int     `gorm:" type:int unsigned not null; comment: 销售量"`
-	Nums              int     `gorm:" type:tinyint not null; default: -1; comment: 限购 -1无限"`
-	Used              int     `gorm:" type:int unsigned not null; comment: 已使用"`
-	Total             int     `gorm:" type:int unsigned not null; comment: 总数"`
-	Data              string  `gorm:" type:text; comment: 数据"`
-	Describes         string  `gorm:" type:text; comment: 描述"`
-	UpdatedAt         int     `gorm:" type:int unsigned not null; autoUpdateTime; comment: 更新时间"`
-	CreatedAt         int     `gorm:" type:int unsigned not null; autoCreateTime; comment: 创建时间"`
-}
-
-// GetProductDefault 获取默认数据
-func GetProductDefault() *Product {
-	return &Product{
-		Id:        0,
-		Name:      gofakeit.Name(),
-		Images:    gofakeit.ImageURL(200, 400),
-		Money:     gofakeit.Float64Range(10, 30),
-		Type:      gofakeit.RandomInt([]int{1, 3}),
-		Sort:      gofakeit.Number(1, 100),
-		Status:    gofakeit.RandomInt([]int{-2, -1, 20}),
-		Recommend: gofakeit.RandomInt([]int{-1, 20}),
-		Sales:     gofakeit.Number(1, 500),
-		Nums:      -1,
-		Used:      gofakeit.Number(1, 500),
-		Total:     gofakeit.Number(500, 600),
-		Data:      gofakeit.Letter(),
-		Describes: gofakeit.Letter(),
-	}
-}
-
-// SetAdminId 设置管理员Id
-func (p *Product) SetAdminId(id int) *Product {
-	p.AdminUserId = id
-	return p
-}
-
-// SetProductCategory 设置分类Id
-func (p *Product) SetProductCategory(id int) *Product {
-	p.ProductCategoryId = id
-	return p
-}
-
-// SetWalletAssetsId 设置钱包资产Id
-func (p *Product) SetWalletAssetsId(id int) *Product {
-	p.WalletAssetsId = id
-	return p
+	gorm.Model
+	AdminId    uint        `gorm:"type:int unsigned not null;comment:管理员ID"`
+	ParentId   uint        `gorm:"type:int unsigned not null;comment:父级ID"`
+	StoreId    uint        `gorm:"type:int unsigned not null;comment:店铺ID"`
+	CategoryId uint        `gorm:"type:int unsigned not null;comment:类目ID"`
+	AssetsId   uint        `gorm:"type:int unsigned not null;comment:资产ID"`
+	Name       string      `gorm:"type:varchar(512) not null;comment:标题"`
+	Images     GormStrings `gorm:"type:varchar(4096) not null;comment:商品图片"`
+	Money      float64     `gorm:"type:decimal(20,6) not null;comment:标价"`
+	Discount   float64     `gorm:"type:decimal(8,4) not null;comment:折扣"`
+	Type       int         `gorm:"type:tinyint not null;default:1;comment:类型1店铺商品 2批发商品"`
+	Sort       int         `gorm:"type:tinyint not null;default:99;comment:排序"`
+	Sales      int         `gorm:"type:int unsigned not null;default:0;comment:销售量"`
+	Status     int         `gorm:"type:tinyint not null;default:10;comment:状态-1禁用 10启用"`
+	Data       string      `gorm:"type:text;comment:数据"`
+	Desc       string      `gorm:"type:text;comment:描述"`
 }
