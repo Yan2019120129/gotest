ec057907ec04b34fc4b5a067b64b2869e0375e62
Yan2019120129 <1556403682@qq.com>
2024-06-06 10:44:07 +0800

    整理公共文件

--- main.go
+++ main.go
@@ -1,6 +1,31 @@
 // This file is auto-generated, don't edit it. Thanks.
 package main
 
-func main() {
+import (
+	"fmt"
+	"gotest/common/utils"
+)
 
+func main() {
+	for i := 0; i < 5; i++ {
+		fmt.Println(OrderRandomPrice(2367.33, 1, 2))
+	}
 }
+
+// OrderRandomPrice 随机订单价格 1涨 2跌
+func OrderRandomPrice(currentPrice float64, mode, accuracy int) float64 {
+	denominator := 1.0
+	for i := 0; i < accuracy; i++ {
+		denominator = denominator * 10
+	}
+	randomNumber := float64(utils.NewRandom().Intn(1, 9)) / denominator
+	switch mode {
+	case 1:
+		currentPrice += randomNumber
+	case 2:
+		currentPrice -= randomNumber
+	}
+	return currentPrice
+}
+
+// 100288.478
