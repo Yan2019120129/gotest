975c5518f9bdfc73e98d0cf4d284224316634fd5
Yan2019120129 <1556403682@qq.com>
2024-06-06 21:42:17 +0800

    language test

--- base/language_t/index_test.go
+++ base/language_t/index_test.go
@@ -1,1 +1,18 @@
 package language_t
+
+import (
+	"fmt"
+	"golang.org/x/text/language"
+	"golang.org/x/text/message"
+	"testing"
+)
+
+// TestLanguage 测试基础语言包的作用
+func TestLanguage(t *testing.T) {
+	err := message.SetString(language.Chinese, "%s is %d years old", "%s 今年 %d 岁了")
+	if err != nil {
+		return
+	}
+	msg := message.Render("%s is %d years old", "张三")
+	fmt.Println(msg)
+}
