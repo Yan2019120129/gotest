c99dc9dcaee590faf359436042dfb4cdafceeb4e
Yan2019120129 <1556403682@qq.com>
2024-06-07 11:50:32 +0800

    更新使用1.21版本

--- frame/my-fiber/go.mod
+++ frame/my-fiber/go.mod
@@ -1,6 +1,6 @@
 module my-fiber
 
-go 1.20
+go 1.21
 
 require (
 	github.com/andybalholm/brotli v1.1.0 // indirect
--- frame/my-frame/models/account.go
+++ frame/my-frame/models/account.go
@@ -1,20 +1,18 @@
 package models
 
-import "github.com/brianvoe/gofakeit/v6"
-
 // Account 账户信息
 type Account struct {
-	AccountNumber int                   `json:"account_number"`
-	Address       *gofakeit.AddressInfo `json:"address"`
-	Age           int                   `json:"age"`
-	Balance       int                   `json:"balance"`
-	City          string                `json:"city"`
-	Email         string                `json:"email"`
-	Employer      string                `json:"employer"`
-	Firstname     string                `json:"firstname"`
-	Gender        string                `json:"gender"`
-	Lastname      string                `json:"lastname"`
-	State         string                `json:"state"`
+	AccountNumber int    `json:"account_number"`
+	Address       string `json:"address"`
+	Age           int    `json:"age"`
+	Balance       int    `json:"balance"`
+	City          string `json:"city"`
+	Email         string `json:"email"`
+	Employer      string `json:"employer"`
+	Firstname     string `json:"firstname"`
+	Gender        string `json:"gender"`
+	Lastname      string `json:"lastname"`
+	State         string `json:"state"`
 }
 
 type Address struct {
