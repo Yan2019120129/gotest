d81f31cfaf4b3ff60d6bf6dcdc9a0c87366317e2
Yan2019120129 <1556403682@qq.com>
2024-08-02 09:22:04 +0800

    添加mysql查询案例

--- middleware/mysql_t/select_test.go
+++ middleware/mysql_t/select_test.go
@@ -1,1 +1,43 @@
 package mysql_t
+
+import (
+	"gotest/common/models"
+	"gotest/common/module/gorm/database"
+	"testing"
+)
+
+// SelectAllField 全字段
+func BenchmarkSelectAllField(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		userInfo := models.User{}
+		database.DB.Where("id = ?", 1).Find(&userInfo)
+	}
+}
+
+// SelectAllField 全字段
+func BenchmarkSelectField(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		username := ""
+		database.DB.Model(&models.User{}).Select("user_name").Where("id = ?", 1).Find(&username)
+	}
+}
+
+// SelectAllField 全字段
+func BenchmarkSelectField1(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		username := ""
+		database.DB.Model(&models.User{}).Select("user_name").Where("id = ?", 1).Scan(&username)
+	}
+}
+
+// SelectAllField 全字段
+func BenchmarkSelectField2(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		username := ""
+		database.DB.Raw("select user_name from user where id = ?", 1).Scan(&username)
+	}
+}
