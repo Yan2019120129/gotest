46a95fe5c7cf63f7d39551206a7f4ec08e54ae73
Yan2019120129 <1556403682@qq.com>
2025-03-01 16:01:12 +0800

    添加反射工具

--- main.go
+++ main.go
@@ -1,4 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"gotest/common/models"
+	"gotest/common/utils"
+)
+
 func main() {
+	userInfo := models.User{}
+	reflectInstance := utils.NewReflectModel(userInfo)
+	modelsName := reflectInstance.GetName()
+	modelsName = utils.CamelToSnake(modelsName)
+	fields := reflectInstance.GetFields()
+	for i, field := range fields {
+		fields[i] = utils.CamelToSnake(field)
+	}
+	tag := reflectInstance.GetFieldsDesc("status", "gorm", "comment:")
+	fmt.Println(modelsName, fields, tag)
 }
